11ed6c8257fd78a7eef7f547ed11afe0cda45e24
AL AMIN <123535174+malamin24@users.noreply.github.com>
2023-03-22 20:11:16 +0600

    Update defults.js

--- js/defults.js
+++ js/defults.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
     $(".toggole").click(function(){
-      $(".menu").css({
-      'margin-left' : "-3%"
+      $(".menu").animate(
+        {
+        'margin-left': "-3%"
+        }
+      );
+        
+    });
+    
+  });
+  $(document).ready(function(){
+    $(".menu").click(function(){
+      $(".menu").animate(
+        {
+        'margin-left': "-120%"
+        }
+      );
+        
+    });
+    
+  });
+
+
+
+  $(document).ready(function(){
+    $(".slider_left_arrow").click(function(){
+      $(".slider").animate({
+        left: '+=-50%',
+        trnslate: 'slow',
       });
+        
+    });
+    $(".slider_right_arrow").click(function(){
+      $(".slider").animate({
+        left: '+=+50%',
+        trnslate: 'slow',
+      }
+      );
         
     });
     
